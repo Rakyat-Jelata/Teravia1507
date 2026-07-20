d2a0e52cf625f423d0bc3936195f5bb1ced3181f
Rakyat-Jelata <rjelata012@gmail.com>
2026-07-20 23:22:00 +0700

    Add loadDashboard function to fetch user profile
    Implement dashboard loading functionality to fetch and display user profile data.

---
+++ assets/js/dashboard.js
@@ -0,0 +1,84 @@
+/* ==========================================================
+   TERAVIA
+   Dashboard
+   assets/js/dashboard.js
+========================================================== */
+
+
+async function loadDashboard(){
+
+
+    const user =
+    await getCurrentUser();
+
+
+    if(!user) return;
+
+
+
+    // Ambil profile
+    const { data: profile, error } =
+
+    await supabaseClient
+    .from('profiles')
+    .select('*')
+    .eq('id', user.id)
+    .single();
+
+
+
+    if(error){
+
+        console.error(error);
+        return;
+
+    }
+
+
+
+    // Tampilkan data user
+
+    const nama =
+    document.getElementById('namaUser');
+
+
+    if(nama){
+
+        nama.textContent =
+        profile.nama_lengkap;
+
+    }
+
+
+
+    const jenis =
+    document.getElementById('jenisAkun');
+
+
+    if(jenis){
+
+        jenis.textContent =
+        profile.jenis_akun;
+
+    }
+
+
+
+    const role =
+    document.getElementById('roleUser');
+
+
+    if(role){
+
+        role.textContent =
+        profile.role;
+
+    }
+
+
+
+}
+
+
+
+loadDashboard();
